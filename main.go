b5ed456886743bed40557fba1f6180feaa569dd7
Daniel Aguila <danielbaguila@gmail.com>
2025-05-04 14:07:16 -0500

    set to args

--- main.go
+++ main.go
@@ -9,17 +9,27 @@ func manual() {
 	fmt.Println("\n      Welcome to Capyfilter\n" +
 		"----parameters - explanation----\n" +
 		"a -  sort directory by alphabetical order" +
-		"" +
-		"\n")
+		"")
+	return
 }
 
 func filterAlphabetical(filesDir []os.DirEntry) {
 	fmt.Println("Filtering by alphabetical order...")
 }
 
+func mapArgs(args []string) map[string]bool {
+	set := make(map[string]bool)
+	for _, arg := range args {
+		set[arg] = true
+	}
+	return set
+}
+
 func main() {
-	fmt.Println(os.Args)
-	if os.Args[1] == "-h" || os.Args[1] == "--help" {
+	var argSet map[string]bool
+
+	argSet = mapArgs(os.Args[1:])
+	if argSet["-h"] || argSet["--help"] {
 		manual()
 	}
 }
