e6fa7af9f3035af7ea4c6f0b67b64185dc94ed2f
Daniel Aguila <danielbaguila@gmail.com>
2025-05-04 16:04:32 -0500

    refactor get

--- main.go
+++ main.go
@@ -2,27 +2,34 @@ package main
 
 import (
 	"fmt"
-	//tea "github.com/charmbracelet/bubbletea"
 	"log"
 	"net/http"
 	"os"
 	"strings"
 )
 
+func getAPI(url string) {
+
+	if !strings.HasPrefix(url, "http") {
+		url = "http://" + url
+	}
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	header := res.Header.Get("Content-Type")
+	if strings.Contains(header, "application/json") {
+
+	}
+
+	defer res.Body.Close()
+	fmt.Println(res.StatusCode)
+}
+
 func main() {
 	method := os.Args[1]
 	if method == "get" {
 		url := os.Args[2]
-
-		if !strings.HasPrefix(url, "http") {
-			url = "http://" + url
-		}
-		res, err := http.Get(url)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		defer res.Body.Close()
-		fmt.Println(res.Status)
+		getAPI(url)
 	}
 }
